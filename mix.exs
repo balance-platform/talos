4c23abd53c8a096ca1f736c1fc80e4b09d851843
Dmitry Shpagin <sofakingcode@gmail.com>
2019-11-20 22:34:29 +0300

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Talos.MixProject do
   def project do
     [
       app: :talos,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
