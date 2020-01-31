70759e98e5a98df575121e9e1faa8805fd954a7f
d.shpagin <d.shpagin@fun-box.ru>
2020-02-01 02:41:20 +0300

    Allow older elixir versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Talos.MixProject do
     [
       app: :talos,
       version: "1.0.5",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
