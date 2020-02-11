0da605fe13bbd0f4572f7bec1a342bfc168d0979
Dmitry Shpagin <d.shpagin@balance-pl.ru>
2020-02-12 00:13:48 +0300

    up version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Talos.MixProject do
   def project do
     [
       app: :talos,
-      version: "1.0.5",
+      version: "1.1.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
