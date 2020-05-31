c5881e917a5fa821b54156a0ca0826101cfc4736
Dmitry <dmitry.shpagin@balance-pl.ru>
2020-05-31 19:11:33 +0300

    FixedType

---
+++ lib/types/fixed_type.ex
@@ -0,0 +1,44 @@
+defmodule Talos.Types.FixedType do
+  @moduledoc ~S"""
+  Fixed type is used to check value to be equal expected
+
+  For example:
+  ```elixir
+    
+    iex> the_4th_planet = %Talos.Types.FixedType{value: "Mars"}
+    iex> Talos.valid?(the_4th_planet, "Earth")
+    false
+    iex> Talos.valid?(the_4th_planet, "Mars")
+    true
+    
+  ```
+
+
+  Additional parameters:
+  `allow_nil` - allows value to be nil
+  """
+  defstruct [:value, allow_nil: false]
+
+  @type t :: %{
+          __struct__: atom,
+          allow_nil: boolean,
+        }
+
+  @behaviour Talos.Types
+
+  @spec valid?(Talos.Types.FixedType.t(), any) :: boolean
+  def valid?(type, value) do
+    errors(type, value) == []
+  end
+
+  def errors(%__MODULE__{allow_nil: true}, nil) do
+    []
+  end
+
+  def errors(%__MODULE__{value: expected_value} = expected, value)  do
+    case expected_value == value do
+      true -> []
+      false -> [inspect(value), "Should be #{inspect(expected)}"]
+    end
+  end
+end
---
+++ test/types/fixed_type_test.exs
@@ -0,0 +1,18 @@
+defmodule Talos.Types.FixedTypeTest do
+  use ExUnit.Case
+  alias Talos.Types.FixedType
+
+  doctest FixedType
+
+  test "#valid?" do
+    assert true == FixedType.valid?(%FixedType{value: "A"}, "A")
+    assert false == FixedType.valid?(%FixedType{value: "A"}, "B")
+    assert false == FixedType.valid?(%FixedType{value: "A"}, nil)
+    assert true == FixedType.valid?(%FixedType{value: "A", allow_nil: true}, nil)
+  end
+
+  test "errors - returns list with error element" do
+    assert [] == FixedType.errors(%FixedType{value: "A"}, "A")
+    assert [_B, _should_be_eq_to_type] = FixedType.errors(%FixedType{value: "A"}, "B")
+  end
+end
