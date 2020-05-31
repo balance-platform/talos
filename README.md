89b24e8e4359458cfd4c21ed03dda1d587c84718
Dmitry <dmitry.shpagin@balance-pl.ru>
2020-05-31 20:26:41 +0300

    Readme Update

--- README.md
+++ README.md
@@ -100,7 +100,7 @@ Talos.valid?(ZipCodeType, 123456) # => false
 ```elixir
 def deps do
   [
-    {:talos, "~> 1.0"}
+    {:talos, "~> 1.2"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Talos.MixProject do
     [
       app: :talos,
       version: "1.2.1",
-      elixir: ">= 1.8",
+      elixir: ">= 1.8.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
